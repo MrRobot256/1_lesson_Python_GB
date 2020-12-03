10809e74a64c5044fe0fbdff3e75de71750e4fd9
MrRobot256 <56654570+MrRobot256@users.noreply.github.com>
2020-12-03 15:00:59 +0300

    Delete task2.py

--- 4_lesson/task2.py
+++
@@ -1,7 +0,0 @@
-# Задание 2
-
-my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-print(my_list)
-new_list = [my_list[number] for number in range(1, len(my_list)) if my_list[number] > my_list [number - 1]]
-
-print(new_list)
